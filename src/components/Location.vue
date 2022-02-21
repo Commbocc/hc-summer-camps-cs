6e67c796b741b4416b18e5bee2c0f433f69eb99c
Kevin McCarthy <mccarthyk@hcflgov.net>
2022-02-21 12:43:03 -0500

    add phone number and lunch availability

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -3,7 +3,10 @@ const props = defineProps({
   location: Object,
 })
 
-const getField = (field: string) => props.location!.fields[field]?.[0]
+const getField = (field: string) => {
+  const fieldValue = props.location!.fields[field]
+  return Array.isArray(fieldValue) ? fieldValue[0] : fieldValue
+}
 </script>
 
 <template>
@@ -14,7 +17,13 @@ const getField = (field: string) => props.location!.fields[field]?.[0]
   >
     <div class="d-flex w-100 justify-content-between">
       <p class="h6 mb-1" v-text="getField('LocationName')"></p>
+      <small
+        v-if="getField('LunchAvailable')"
+        class="badge rounded-pill bg-secondary"
+        >Lunch Available</small
+      >
     </div>
     <p class="mb-1" v-text="getField('LocationAddress')"></p>
+    <small v-text="getField('LocationPhone')"></small>
   </a>
 </template>
